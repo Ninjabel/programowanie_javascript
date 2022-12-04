c4463949f255ff63387ede2d9d206dbf1c8416ed
Ninjabel <85708915+Ninjabel@users.noreply.github.com>
2022-12-04 22:39:48 +0100

    lab4_homework_v3 (+new mechanics)

--- lab4/main.js
+++ lab4/main.js
@@ -1,4 +1,6 @@
 const addNoteButton = document.getElementById('add-note-btn');
+const plusSvgWrap = document.createElement('div');
+const plusSvgImg = document.createElement('img');
 const titleInput = document.createElement('input');
 const notesCon = document.getElementById('notes-con');
 const notesConText = document.getElementById('notes-con-text');
@@ -7,6 +9,25 @@ const notepadWin = document.getElementById('notepad-window');
 const saveNoteBtn = document.createElement('button');
 const colorsCon = document.getElementById('colors-con');
 const colorArr = document.querySelectorAll('.color');
+
+const note = document.createElement('div');
+const pinNoteBtn = document.createElement('div');
+const pinNoteBtnIMG = document.createElement('img');
+const noteShortTitle = document.createElement('div');
+const shortTitleText = document.createElement('p');
+const dateTitle = document.createElement('div');
+const noteEditBtn = document.createElement('div');
+const noteEditBtnIMG = document.createElement('img');
+const noteDeleteBtn = document.createElement('div');
+const noteDeleteBtnIMG = document.createElement('img');
+
+
+const notesTitleArr = [];
+const notesDescArr = [];
+const notesDateArr = [];
+var notesCounter = 0;
+
+
 
 addNoteButton.addEventListener('click', addNote);
 saveNoteBtn.addEventListener("click", saveNote);
@@ -73,6 +94,7 @@ function addNote() {
     addNoteButton.appendChild(titleInput);
     titleInput.classList.add('title-input', 'no-outline');
     titleInput.setAttribute('placeholder', 'Note title');
+    titleInput.setAttribute('maxlength', '50');
     titleInput.focus();
     notesConText.style.height = "0px";
     notesCon.style.height = "450px";
@@ -89,12 +111,63 @@ function saveNote() {
     addNoteButton.classList.add('add-note-bttn');
     addNoteButton.classList.remove('note-title-con');
     addNoteButton.textContent = 'Add new note';
+    addNoteButton.appendChild(plusSvgWrap);
+    plusSvgWrap.classList.add('plus-svg');
+    plusSvgWrap.appendChild(plusSvgImg);
+    plusSvgImg.setAttribute('src', 'icons/plus-solid.svg');
+    addNoteButton.appendChild
     titleInput.remove();
     notesCon.style.height = "535px";
     noteDescArea.remove();
     saveNoteBtn.remove();
     colorsCon.classList.remove('colors-container');
-    addNoteButton.style.border = "1px solid white";
-    notesCon.style.border = "1px solid white";
-    notepadWin.style.border = "1px solid white";
+    notesConText.style.height = "18px";
+    addNoteButton.style.border = "1px solid grey";
+    notesCon.style.border = "1px solid grey";
+    notepadWin.style.border = "2px solid white";
+    saveNoteToMemory();
+    titleInput.value = "";
+    noteDescArea.value = "";
+    createNote();
+}
+
+function saveNoteToMemory() {
+    const titleInputSave = titleInput.value;
+    const noteDescAreaSave = noteDescArea.value;
+    const date = new Date();
+    const dateDisplay = date.toLocaleString('pl');
+    notesTitleArr[notesCounter] = titleInputSave;
+    notesDateArr[notesCounter] = dateDisplay;
+}
+
+function createNote() {
+    notesCon.appendChild(note);
+    note.classList.add('note');
+
+    note.appendChild(pinNoteBtn);
+    pinNoteBtn.classList.add('pin-note-bttn');
+    pinNoteBtn.appendChild(pinNoteBtnIMG);
+    pinNoteBtnIMG.setAttribute('src', 'icons/star-regular.svg')
+
+    note.appendChild(noteShortTitle);
+    noteShortTitle.classList.add('note-short-title');
+    
+    noteShortTitle.appendChild(dateTitle);
+    dateTitle.classList.add('date');
+    dateTitle.innerText = notesDateArr[notesCounter];
+
+    noteShortTitle.appendChild(shortTitleText);
+    shortTitleText.innerText = notesTitleArr[notesCounter];
+
+    note.appendChild(noteEditBtn);
+    noteEditBtn.classList.add('note-edit-bttn');
+    noteEditBtn.appendChild(noteEditBtnIMG);
+    noteEditBtnIMG.setAttribute('src', 'icons/pencil-solid.svg')
+
+    note.appendChild(noteDeleteBtn);
+    noteDeleteBtn.classList.add('note-delete-bttn');
+    noteDeleteBtn.appendChild(noteDeleteBtnIMG);
+    noteDeleteBtnIMG.setAttribute('src', 'icons/xmark-solid.svg')
+
+    notesCounter++;
 }
