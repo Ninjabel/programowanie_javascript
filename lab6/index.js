c65cc5f3b453ed04eec62e5e484e00655df5d7bc
Ninjabel <85708915+Ninjabel@users.noreply.github.com>
2023-01-07 18:16:02 +0100

    clear all lab4

--- lab6/index.js
+++ lab6/index.js
@@ -2,17 +2,40 @@ window.addEventListener('deviceorientation', onDeviceMove)
 
 const ball = document.getElementById("ball");
 const hole = document.getElementById("hole");
+const gardeb = document.getElementById("garden");
+
+const maxX = garden.clientWidth - ball.clientWidth;
+const maxY = garden.clientHeight - ball.clientHeight;
 
 function onDeviceMove(event) {
-    console.log(event)
+
+    var x = event.beta; // In degree in the range [-180,180)
+    var y = event.gamma; // In degree in the range [-90,90)
+
+    console.log(x, y);
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x > 90) {
+    x = 90;
+  }
+  if (x < -90) {
+    x = -90;
+  }
+
+  // To make computation easier we shift the range of
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  animate();
 }
 
-function animate() {
+function animate() {    
+    console.log('works');
 
-    let alpha;
-    let beta;
-
-    requestAnimationFrame(animate)
+    ball.style.top = `${(maxY * y) / 180 - 10}px`;
+    ball.style.left = `${(maxX * x) / 180 - 10}px`;
 }
 
-requestAnimationFrame(animate)
+requestAnimationFrame(animate);
