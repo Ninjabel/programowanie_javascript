81fbe6f3f681de8c4dca3fa9ca980ebf50261d82
Ninjabel <85708915+Ninjabel@users.noreply.github.com>
2023-01-20 22:37:08 +0100

    lab3 +func for all buttons

--- lab3/main.js
+++ lab3/main.js
@@ -22,26 +22,37 @@ const start2 = document.querySelector('#start2');
 const start3 = document.querySelector('#start3');
 const start4 = document.querySelector('#start4');
 
+const starts = [start1, start2, start3, start4];
+starts.forEach(start => {
+    start.addEventListener('click', this.startRecording);
+});
+
 const stop1 = document.querySelector('#stop1');
 const stop2 = document.querySelector('#stop2');
 const stop3 = document.querySelector('#stop3');
 const stop4 = document.querySelector('#stop4');
+
+const stops = [stop1, stop2, stop3, stop4];
+stops.forEach(stop => {
+    stop.addEventListener('click', stopRecording);
+});
 
 const play1 = document.querySelector('#play1');
 const play2 = document.querySelector('#play2');
 const play3 = document.querySelector('#play3');
 const play4 = document.querySelector('#play4');
 
+const plays = [play1, play2, play3, play4];
+
+plays.forEach(play => {
+    play.addEventListener('click', playRecording);
+});
+
+let records = [];
 let startTime;
 let endTime;
 let pressedKeys = [];
 let recording = false;
-
-start1.addEventListener('click', startRecording);
-stop1.addEventListener('click', stopRecording);
-play1.addEventListener('click', playRecording);
-
-
 
 function startRecording() {
     startTime = Date.now();
@@ -90,5 +101,3 @@ function playRecording() {
     }
     playNext();
 }
-
-
